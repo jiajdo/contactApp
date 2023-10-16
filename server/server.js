eeb73d78ea6b1fe71ae948c0924bc560006cc29d
Jia <jovsofjdo@gmail.com>
2023-10-16 07:50:32 -0700

    Added backend route to create events

--- server/server.js
+++ server/server.js
@@ -15,6 +15,7 @@ app.get('/', (req,res)=> {
     res.send("Your home page works!");
 })
 
+//showing contact
 app.get("/contacts", async(req, res) => {
     try {
         const {rows: contacts} = await db.query (`SELECT * FROM contacts`)
@@ -28,8 +29,48 @@ app.get("/contacts", async(req, res) => {
     }
 })
 
+//adding contacts
+app.post("/createContact", async (req, res) => {
+  
+    try {
+      const createContact = {
+        name: req.body.name,
+        email:  req.body.email,
+        phone: req.body.phone,
+        notes: req.body.notes
+      };
+      const result = await db.query(
+        "INSERT INTO contacts(name, email, phone, notes) VALUES ($1, $2, $3, $4) RETURNING *",
+        [createContact.name, createContact.email, createContact.phone, createContact.notes]
+      );
+      let response = result.rows[0];
+      console.log(response);
+      res.json(response);
+    } catch (e) {
+      console.log(error);
+      return res.status(400).json({ error });
+    }
+  });
 
-
+  app.post("/api/delete", async (req, res) => {
+    try {
+      const deleteEvent = {
+        title: req.body.title,
+        location: req.body.location,
+        eventtime: req.body.eventtime,
+      };
+      const deleteResult = await db.query(
+        "DELETE FROM events WHERE title=$1 AND location=$2 AND eventtime=$3 RETURNING *",
+        [deleteEvent.title, deleteEvent.location, deleteEvent.eventtime]
+      );
+      let response = deleteResult.rows[0];
+      console.log(response);
+      res.json(response);
+    } catch (error) {
+      console.log(error);
+      return res.status(400).json({ error });
+    }
+  });
 
 app.listen(PORT, () => {
     console.log(`Your man is at ${PORT}`)
