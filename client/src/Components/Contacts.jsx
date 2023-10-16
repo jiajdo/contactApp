663c9e9daef8192ef89d7cf45464070fc8a5cfa2
Jia <jovsofjdo@gmail.com>
2023-10-16 08:20:39 -0700

    Added Create-Contact import statement

--- client/src/Components/Contacts.jsx
+++ client/src/Components/Contacts.jsx
@@ -2,12 +2,14 @@
 
 import { useState } from "react";
 import ViewContact from "./View-Contact";
+import CreateContact from "./Create-Contact";
+
 
 const Contacts = (props) => {
     console.log({props}, 'contacts props')
   const renderContacts = props.contacts.map((contact) => {
     console.log({contact})
-    return <ViewContact contact={contact}></ViewContact>;
+    return <ViewContact contact={contacts}></ViewContact>;
   });
   return <div>{renderContacts}</div>;
 };
